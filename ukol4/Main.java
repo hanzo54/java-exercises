6896b1d85a54ee4299176cc42ec72434a65e4f8e
hanzo54 <doubekjan@icloud.com>
2025-03-11 12:59:18 +0100

    poupraveno

--- ukol4/Main.java
+++ ukol4/Main.java
@@ -10,14 +10,24 @@ public class Main
 
         PaymentProcessor paymentProcessor = new PaymentProcessor();
 
-        paymentProcessor.pay(creditCardPayment, 100);
-        paymentProcessor.pay(payPalPayment, 0.34);
-        paymentProcessor.pay(bankTransferPayment, 0);
-        
         NoDiscount noDiscount = new NoDiscount();
         noDiscount.applyDiscount(0);
 
         PercentageDiscount percentageDiscount = new PercentageDiscount(30);
         percentageDiscount.applyDiscount(30);
+
+        paymentProcessor.pay(creditCardPayment, 100);
+        if (percentageDiscount < 0)
+        {
+            System.out.println(percentageDiscount);
+        }
+        else
+        {
+            System.out.println(noDiscount);
+        }
+        
+        paymentProcessor.pay(payPalPayment, 0.34);
+        
+        paymentProcessor.pay(bankTransferPayment, 0);
     }  
 }
